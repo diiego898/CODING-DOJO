8427d18b48d76fd1222a63e21c2c8deef0a72f29
diiego898 <diego.quinteros.bascunan@alumnos.sip.cl>
2022-04-21 08:51:25 -0400

    Desafío de código opcional: JavaScript

---
+++ Quinteros_Diego_JS_jugueteo.js
@@ -0,0 +1,9 @@
+function muestraSiElNinoPuedeSubirALaMontañaRusa (EntradaAlturaEdad){
+    // EntradaAlturaEdad = 60;
+
+    if (EntradaAlturaEdad > 52 )
+    console.log('¡Súbete, chico!');
+    else (EntradaAlturaEdad < 52)
+    console.log('Lo siento, chico. Tal vez el próximo año');
+
+}
